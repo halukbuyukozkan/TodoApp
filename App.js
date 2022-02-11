fbc010df339724cf9efa5629114e6deeeb329d2a
halukbuyukozkan <halukbuyukozkan@gmail.com>
2022-02-11 20:15:35 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import Alanı
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+
+
+import { StatusBar } from 'expo-status-bar';
+import { Text, TextInput,Button , View } from 'react-native';
+
+
+// create Alanı
+
+class App extends React.Component{
+  state = {
+    text: "",
+    todo: ""
+  }
+  addTodo = () =>{
+    this.setState({todo: this.state.text});
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Hi World</Text>
+        <TextInput 
+          style={styles.inputStyle}
+          onChangeText={(text)=>this.setState({text})}
+        />
+        <Button
+          title='Add todo'
+          onPress={this.addTodo}
+        />
+        <Text>{this.state.todo}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff',  
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  inputStyle:{
+    height: 40,
+    width: 160,
+    borderColor:"green",
+    borderWidth: 1
+  }
+};
+
+// export Alanı
+export default App;
